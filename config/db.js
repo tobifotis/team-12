dd28c120b7760b24cb491f7835539e905f5096a4
Gregory O <gregvoganesyan@gmail.com>
2026-04-21 00:39:25 -0500

    change db for render
    Added SSL configuration and port setting to the database connection pool.

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
-require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-// connection settings from .env file
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-// connection pool settings
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
